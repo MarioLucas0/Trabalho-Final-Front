09b3e0db153c7ddd652ed32de465a663c7fd547e
Lucas <mariolucas.2000@outlook.com>
2022-10-01 13:03:49 -0300

    Co-authored-by: Rodrigo Lisboa Alves <RodrigoRLA@users.noreply.github.com> Co-authored-by: BrunoJaniques <BrunoJaniques@users.noreply.github.com> Co-authored-by: Pedro Henrique Almeia Burger <burgerpedro@users.noreply.github.com>

--- src/Login/login.js
+++ src/Login/login.js
@@ -18,21 +18,9 @@ function logar() {
   .then(response => { 
 
        var contador = 0;
+       
        for(var i = 0; i < response.length; i++) { 
 
-<<<<<<< HEAD:src/Login/login.js
-            alert('Bem vindo(a)!', alt = "Login realizado com suceeso");
-            location.href = "../Home/index.html";
-            ctx.beginPath()
-         
-            
-         } else {
-            alert('Usuario ou senha incorretos!', alt = "usuario ou senha invalidos.");
-            ctx.beginPath()  
-         } 
-      }) 
-       
-=======
         console.log(response[i])
          
         if (response[i].usuario === usuario && response[i].senha === senha) {
@@ -45,7 +33,6 @@ function logar() {
        if(contador === 0) {
          alert('Usuario ou senha incorretos!', alt = "usuario ou senha invalidos.");
        }       
->>>>>>> 6e7e370d119d8cb285209416a18068a4cd7d9e4a:js/login.js
   })
 
 
